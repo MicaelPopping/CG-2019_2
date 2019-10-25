584c333f915112b8aebc34b8923e182882077357
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-10-25 19:36:19 -0300

    terminei fundamentals

--- webgl2_fundamentals/1_Fundamentals/1_fundamentals/script.js
+++ webgl2_fundamentals/1_Fundamentals/1_fundamentals/script.js
@@ -28,6 +28,46 @@ void main() {
 }
 `;
 
+/**
+ * Cria e compila o shader.
+ */
+function createShader(gl, type, source) {
+
+    var shader = gl.createShader(type);
+
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
+    if(success)
+        return shader;
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return undefined;
+}
+
+/**
+ * Cria o programa e faz o link dos 2 shaders.
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+
+    var program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+    if(success)
+        return program;
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+    return undefined;
+}
+
 function main() {
 
     // Obtendo o elemento canvas do html.
@@ -109,44 +149,6 @@ function main() {
     var offset = 0;
     var count = 3;
     gl.drawArrays(primitiveType, offset, count);
-}
-
-/**
- * Cria e compila o shader.
- */
-function createShader(gl, type, source) {
-
-    var shader = gl.createShader(type);
-
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-
-    if(success)
-        return shader;
-
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-}
-
-/**
- * Cria o programa e faz o link dos 2 shaders.
- */
-function createProgram(gl, vertexShader, fragmentShader) {
-
-    var program = gl.createProgram();
-
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-
-    if(success)
-        return program;
-
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
 }
 
 main()
