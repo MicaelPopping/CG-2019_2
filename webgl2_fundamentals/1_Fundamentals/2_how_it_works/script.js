bc6a85361197432e4b645c3df38e273d251e9afc
Micael Popping <mmpopping@inf.ufpel.edu.br>
2019-10-26 12:34:44 -0300

    Finished 2_how_it_work

--- webgl2_fundamentals/1_Fundamentals/2_how_it_works/script.js
+++ webgl2_fundamentals/1_Fundamentals/2_how_it_works/script.js
@@ -1,72 +1,33 @@
 var vertexShaderSource = `#version 300 es
      
-// um atributo é um input (in) para um vertex shader.
-// ele receberá dados de um buffer
-in vec4 a_position;
- 
-// todos os shaders possuem uma função main
+in vec2 a_position;
+in vec4 a_color;
+
+uniform mat3 u_matrix;
+
+out vec4 v_color;
+
 void main() {
- 
-// gl_Position é uma variável especial de um vertex shader
-// é responsável pela configuração
-gl_Position = a_position;
+
+  gl_Position = vec4((u_matrix * vec3(a_position, 1)).xy, 0, 1);
+
+  v_color = a_color;
 }
 `;
  
 var fragmentShaderSource = `#version 300 es
  
-// fragment shaders não tem uma precisão padrão, então nós precisamos
-// escolher uma. mediump é um bom valor padrão. Do Inglês "medium precision", significa "precisão média"
 precision mediump float;
+
+in vec4 v_color;
  
-// precisamos declarar um output para o fragment shader
 out vec4 outColor;
  
 void main() {
-  // Simplesmente defina o output para um constante com uma cor avermelhada-roxa
-  outColor = vec4(1, 0, 0.5, 1);
+
+  outColor = v_color;
 }
 `;
-
-/**
- * Cria e compila o shader.
- */
-function createShader(gl, type, source) {
-
-    var shader = gl.createShader(type);
-
-    gl.shaderSource(shader, source);
-    gl.compileShader(shader);
-    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-
-    if(success)
-        return shader;
-
-    console.log(gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-    return undefined;
-}
-
-/**
- * Cria o programa e faz o link dos 2 shaders.
- */
-function createProgram(gl, vertexShader, fragmentShader) {
-
-    var program = gl.createProgram();
-
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-
-    if(success)
-        return program;
-
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-    return undefined;
-}
 
 function main() {
 
@@ -82,73 +43,120 @@ function main() {
         return;
     }
 
-    // Criando os shaders GLSL. 
-    var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
-    var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
-
     // Fazendo o link dos shaders e criando o programa.
-    var program = createProgram(gl, vertexShader, fragmentShader);
+    var program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
 
-    // Outro modo de linkar os shaders e montar o programa.
-    //var program = webglUtils.createProgramFromSources(gl, [vertexShaderSource, fragmentShaderSource]);
+    //Localizando e guardadno as posições dos atributos.
+    var positionLocation = gl.getAttribLocation(program, "a_position");
+    var colorLocation = gl.getAttribLocation(program, "a_color");
 
-    /** 
-     * Localizando e guardadno a posição do atributo a_position.
-     * Criando um buffer.
-     * Vinculando o buffer de posição. Ponto de ligação.
-     */ 
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-    var positionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    //Localizando e guardadno a posições do uniform.
+    var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
-    // Colocando dados no buffer, referenciando-o através do ponto de ligação.
-    var positions = [
-        0, 0,
-        0, 0.5,
-        0.7, 0,
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-    /**
-     * Cria uma coleção do estado do atributo.
-     * Vincula.
-     */
+    // Cria conjunto de atributos.
     var vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
 
-    // Ativa o atributo. Fala que queremos tirar os dados de um buffer.
-    gl.enableVertexAttribArray(positionAttributeLocation);
+    // Criando um buffer para as posições.
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // Especificando como obter os dados.
+    setGeometry(gl);
+
+    // Informa ao atributo position como extrair os dados do array buffer.
+    gl.enableVertexAttribArray(positionLocation);
     var size = 2; // 2 componentes por iteração.
     var type = gl.FLOAT; // Os dados são floats de 32 bits.
     var normalize = false; // Não normalize os dados.
     var stride = 0; // 0 = mover para frente size * sizeof(type) cada iteração para obter a proxima iteração.
     var offset = 0; // Comece no inicio do buffer.
+    gl.vertexAttribPointer(positionLocation, size, type, normalize, stride, offset);
 
-    gl.vertexAttribPointer(
-        positionAttributeLocation, size, type, normalize, stride, offset
-    );
+    // Criando um buffer para as cores.
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 
-    // Manipula o tamanho da tela. Não entendi muito bem.
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    setColors(gl);
 
-    // Limpar o canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Informa ao atributo color como extrair os dados do array buffer.
+    gl.enableVertexAttribArray(colorLocation);
+    var size = 4; // 2 componentes por iteração.
+    var type = gl.FLOAT; // Os dados são floats de 32 bits.
+    var normalize = false; // Não normalize os dados.
+    var stride = 0; // 0 = mover para frente size * sizeof(type) cada iteração para obter a proxima iteração.
+    var offset = 0; // Comece no inicio do buffer.
+    gl.vertexAttribPointer(colorLocation, size, type, normalize, stride, offset);
 
-    // Fala para usar o programa
-    gl.useProgram(program);
+    var translation = [350, 150];
+    var angleRadians = 0;
+    var scale = [1, 1];
 
-    // Vincula o atributo/buffer que desejamos.
-    gl.bindVertexArray(vao);
+    drawScene();
 
-    // Executando nosso programa.
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 3;
-    gl.drawArrays(primitiveType, offset, count);
+    function drawScene() {
+
+        // Manipula o tamanho da tela. Não entendi muito bem.
+        webglUtils.resizeCanvasToDisplaySize(gl.canvas);
+
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // Limpar o canvas
+        gl.clearColor(0, 0, 0, 0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        // Fala para usar o programa
+        gl.useProgram(program);
+
+        // Vincula o atributo/buffer que desejamos.
+        gl.bindVertexArray(vao);
+
+        // Gerando a matriz
+        var matrix = m3.projection(gl.canvas.clientWidth, gl.canvas.clientHeight);
+        matrix = m3.translate(matrix, translation[0], translation[1]);
+        matrix = m3.rotate(matrix, angleRadians);
+        matrix = m3.scale(matrix, scale[0], scale[1]);
+
+        // Colocando a matriz no uniform.
+        gl.uniformMatrix3fv(matrixLocation, false, matrix);
+
+        // Executando nosso programa.
+        var primitiveType = gl.TRIANGLES;
+        var offset = 0;
+        var count = 6;
+        gl.drawArrays(primitiveType, offset, count);
+    }
 }
 
-main()
+/**
+ * 
+ */
+function setGeometry(gl) {
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            -150, -100,
+             150, -100,
+            -150,  100,
+             150, -100,
+            -150,  100,
+             150,  100,
+        ]),
+        gl.STATIC_DRAW);
+}
+
+function setColors(gl) {
+
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+            Math.random(), Math.random(), Math.random(), 1,
+        ]),
+        gl.STATIC_DRAW);
+}
+
+main();
